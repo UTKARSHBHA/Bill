beae75abfbed2cb1c6eb24f5e4116ac2436d0522
Utkarsh Bhardwaj <ub2092000@gmail.com>
2020-12-06 12:55:36 +0530

    First video done --VK--

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from tkinter import*
+from tkinter import *
 class Bill_App:
 
     def __init__(self, root):
         self.root = root
         self.root.geometry("1350x700+0+0")
-        self.root.title("Billing Software")
-        bg_color = "#074463"
-        title = Label(self.root, text = "Billing Software" , bd = 12 , relief= GROOVE ,bg =  bg_color, fg = "white",font =("times new roman" , 30 , "bold"), pady = 2 ).pack(fill=X)
+        self.root.title("IPEC CAFETERIA")
+        bg_color = "#072763"
+        title = Label(self.root, text = "FAST FOOD CORNER" , bd = 12 , relief= GROOVE ,bg =  bg_color, fg = "white",font =("times new roman" , 30 , "bold"), pady = 2 ).pack(fill=X)
 
         #===================Customer Detail Frame
         F1 = LabelFrame(self.root ,bd = 10 , relief = GROOVE, text = "Customer Details", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
@@ -25,7 +25,7 @@ class Bill_App:
 
 
         #=================Cosmetics Frame ================
-        F2 = LabelFrame(self.root , bd = 10 , relief = GROOVE, text = "Cusmetics", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
+        F2 = LabelFrame(self.root , bd = 10 , relief = GROOVE, text = "Cosmetics", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
         F2.place(x = 5 , y = 180 ,width=325 , height = 380)
 
         bath_lbl = Label(F2 , text="Bath Soap", font = ("times new roman" , 16 , "bold") , bg = bg_color , fg = "lightgreen" ).grid(row = 0 , column = 0 , padx = 10 , pady = 10 , sticky = "w")
@@ -47,7 +47,7 @@ class Bill_App:
         body_tst= Entry(F2 , width=10, font = ("times new roman" , 16 , "bold"), bd = 5 , relief = SUNKEN).grid(row = 5 , column = 1 , padx = 10 , pady = 10)
 
         #=================Grocery Frame ================
-        F3 = LabelFrame(self.root , bd = 10 , relief = GROOVE, text = "Cusmetics", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
+        F3 = LabelFrame(self.root , bd = 10 , relief = GROOVE, text = "Grocery", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
         F3.place(x = 340 , y = 180 ,width=325 , height = 380)
 
         g1_lbl = Label(F3 , text="Rice", font = ("times new roman" , 16 , "bold") , bg = bg_color , fg = "lightgreen" ).grid(row = 0 , column = 0 , padx = 10 , pady = 10 , sticky = "w")
@@ -70,13 +70,13 @@ class Bill_App:
 
         #=================Cold Drink Frame ================
 
-        F4 = LabelFrame(self.root , bd = 10 , relief = GROOVE, text = "Cusmetics", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
+        F4 = LabelFrame(self.root , bd = 10 , relief = GROOVE, text = "Cold Drinks", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
         F4.place(x = 670 , y = 180 ,width=325 , height = 380)
 
         c1_lbl = Label(F4 , text="Maza", font = ("times new roman" , 16 , "bold") , bg = bg_color , fg = "lightgreen" ).grid(row = 0 , column = 0 , padx = 10 , pady = 10 , sticky = "w")
         c1_tst= Entry(F4 , width=10, font = ("times new roman" , 16 , "bold"), bd = 5 , relief = SUNKEN).grid(row = 0 , column = 1 , padx = 10 , pady = 10)
 
-        c2_lbl = Label(F4 , text="Coke", font = ("times new roman" , 16 , "bold") , bg = bg_color , fg = "lightgreen" ).grid(row = 1 , column = 0 , padx = 10 , pady = 10 , sticky = "w")
+        c2_lbl = Label(F4 , text="Pepsi", font = ("times new roman" , 16 , "bold") , bg = bg_color , fg = "lightgreen" ).grid(row = 1 , column = 0 , padx = 10 , pady = 10 , sticky = "w")
         c2_tst= Entry(F4 , width=10, font = ("times new roman" , 16 , "bold"), bd = 5 , relief = SUNKEN).grid(row = 1 , column = 1 , padx = 10 , pady = 10)
 
         c3_lbl = Label(F4 , text="Frooti", font = ("times new roman" , 16 , "bold") , bg = bg_color , fg = "lightgreen" ).grid(row = 2 , column = 0 , padx = 10 , pady = 10 , sticky = "w")
@@ -91,7 +91,49 @@ class Bill_App:
         c6_lbl = Label(F4 , text="Sprite", font = ("times new roman" , 16 , "bold") , bg = bg_color , fg = "lightgreen" ).grid(row = 5 , column = 0 , padx = 10 , pady = 10 , sticky = "w")
         c6_tst= Entry(F4 , width=10, font = ("times new roman" , 16 , "bold"), bd = 5 , relief = SUNKEN).grid(row = 5 , column = 1 , padx = 10 , pady = 10)
 
+        #========Bill Area========
+        F5 = Frame(self.root, bd=10, relief=GROOVE)
+        F5.place(x=1010, y=180, width=330, height=380)
+        bill_title=Label(F5,text="Bill Area",font="arial 15 bold",bd=7,relief=GROOVE).pack(fill=X)
+        scrol_y=Scrollbar(F5,orient=VERTICAL)
+        self.txtarea=Text(F5,yscrollcommand=scrol_y.set)
+        scrol_y.pack(side=RIGHT,fill=Y)
+        scrol_y.config(command=self.txtarea.yview)
+        self.txtarea.pack()
+
+        #========ButtonFrame========
+        F6=LabelFrame(self.root , bd = 10 , relief = GROOVE, text = "Bill Menu", font= ("times new roman", 15 , "bold"),fg = "gold", bg= bg_color)
+        F6.place(x = 0 , y = 560 ,relwidth=1 , height = 140)
+        m1_lbl=Label(F6,text="Total Cosmetic Price",bg=bg_color,fg="white",font=("times new roman",14,"bold")).grid(row=0,column=0,padx=20,pady=1,sticky="w")
+        m1_txt=Entry(F6,width=18,font="arial 10 bold",bd=7,relief=SUNKEN).grid(row=0,column=1,padx=10,pady=1)
+
+        m2_lbl = Label(F6, text="Total Grocery Price", bg=bg_color, fg="white",font=("times new roman", 14, "bold")).grid(row=1, column=0, padx=20, pady=1, sticky="w")
+        m2_txt = Entry(F6, width=18, font="arial 10 bold", bd=7, relief=SUNKEN).grid(row=1, column=1, padx=10, pady=1)
+
+        m3_lbl = Label(F6, text="Total Cold Drinks Price", bg=bg_color, fg="white",font=("times new roman", 14, "bold")).grid(row=2, column=0, padx=20, pady=1, sticky="w")
+        m3_txt = Entry(F6, width=18, font="arial 10 bold", bd=7, relief=SUNKEN).grid(row=2, column=1, padx=10, pady=1)
+
+        c1_lbl = Label(F6, text="Cosmetic Tax", bg=bg_color, fg="white",
+                       font=("times new roman", 14, "bold")).grid(row=0, column=2, padx=20, pady=1, sticky="w")
+        c1_txt = Entry(F6, width=18, font="arial 10 bold", bd=7, relief=SUNKEN).grid(row=0, column=3, padx=10, pady=1)
+
+        c2_lbl = Label(F6, text="Grocery Tax", bg=bg_color, fg="white",
+                       font=("times new roman", 14, "bold")).grid(row=1, column=2, padx=20, pady=1, sticky="w")
+        c2_txt = Entry(F6, width=18, font="arial 10 bold", bd=7, relief=SUNKEN).grid(row=1, column=3, padx=10, pady=1)
+
+        c3_lbl = Label(F6, text="Cold Drinks Tax", bg=bg_color, fg="white",
+                       font=("times new roman", 14, "bold")).grid(row=2, column=2, padx=20, pady=1, sticky="w")
+        c3_txt = Entry(F6, width=18, font="arial 10 bold", bd=7, relief=SUNKEN).grid(row=2, column=3, padx=10, pady=1)
+
+        btn_F=Frame(F6,bg="#072363")
+        btn_F.place(x=750,width=580,height=105)
+
+        total_btn=Button(btn_F,text="Total",bg="cadetblue",fg="black",bd=2,pady=15,width=10,font="arial 15 bold").grid(row=0,column=0,padx=5,pady=5)
+        GBill_btn=Button(btn_F,text="Generate Bill",bg="cadetblue",fg="black",bd=2,pady=15,width=12,font="arial 15 bold").grid(row=0,column=1,padx=5,pady=5)
+        Clear_btn=Button(btn_F,text="Clear",bg="cadetblue",fg="black",bd=2,pady=15,width=10,font="arial 15 bold").grid(row=0,column=2,padx=5,pady=5)
+        Exit_btn=Button(btn_F,text="Exit",bg="cadetblue",fg="black",bd=2,pady=15,width=10,font="arial 15 bold").grid(row=0,column=3,padx=5,pady=5)
 
 root = Tk()
 obj = Bill_App(root)
 root.mainloop()
+
